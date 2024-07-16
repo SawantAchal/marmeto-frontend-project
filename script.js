6d1e6ebbdd0d8ceae35153f4129f2ee0eb71e58f
SawantAchal <achals1718@gmail.com>
2024-07-17 02:18:05 +0530

    Add complete product cards project with category tabs and dyanamic product display

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded' , function() {
+    fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json')
+    .then(response => response.json())
+    .then(data => {
+        const categories = data.categories;
+        categories.forEach(category => {
+            const categoryName = category.category_name.toLowerCase();
+            const container = document.getElementById(categoryName);
+            if (container) {
+                category.category_products.forEach(product => {
+                    const card = document.createElement('div');
+                    card.className = 'product-card';
+                    const discount = ((product.compare_at_price - product.price) / product.compare_at_price) * 100;
+                    card.innerHTML = `
+                        <img src = " ${product.image}" alt="${product.title}">
+                        ${product.badge_text ? `<div class="badge">${product.badge_text}</div>` : ''}
+                        <div class="product-info">
+                            <p>${product.title}</p>
+                            <p>${product.vendor}</p>
+                            <p class="product-price">Rs ${product.price}</p>
+                            <p class="compare-at-price">Rs ${product.compare_at_price}</p>
+                            <p class="discount">${discount.toFixed(2)}% OFF</p>
+                        </div>
+                        <button>Add to Cart</button>
+                    `;
+                    container.appendChild(card);
+                })
+            }
+        });
+        showCategory('men')
+    })
+})
+
+function showCategory(category) {
+    const categories = ['men', 'women', 'kids'];
+    categories.forEach(cat => {
+        const element = document.getElementById(cat);
+        const tab = document.getElementById(`${cat}-tab`);
+        if (element && tab) {
+            if (cat === category) {
+                element.style.display = 'flex';
+                tab.classList.add('active');
+            } else {
+                element.style.display ='none';
+                tab.classList.remove('active');
+            }
+        }
+    })
+}
